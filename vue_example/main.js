4e637012d186739163412e5042e0cda29396d8c1
Warren He <w@oasislabs.com>
2022-02-18 14:41:47 -0800

    vue_example: migrate to vue 3

--- vue_example/main.js
+++ vue_example/main.js
@@ -1,9 +1,7 @@
 /* eslint-disable */
-import Vue from "vue";
+import { createApp, h } from "vue";
 import App from "./App.vue";
 
-Vue.config.productionTip = false;
-
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+createApp({
+  render: () => h(App),
+}).mount("#app");
